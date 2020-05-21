1335e57daa3125227a2ffa92a09eb16901ceed11
oscargithubkellogg <junkoscar00@gmail.com>
2020-05-21 02:26:53 +0000

    update post class

--- app.rb
+++ app.rb
@@ -20,3 +20,32 @@ get "/" do
   puts @events.inspect
   view "events"
 end
+
+get "/events/:id" do
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    @rsvps = rsvps_table.where(:event_id => params["id"]).to_a
+
+    # SELECT COUNT(*) FROM rsvps WHERE event_id=:id AND going=1
+    @count = rsvps_table.where(:event_id => params["id"], :going => true).count
+
+    puts @rsvps.inspect
+    puts @event.inspect
+    view "event"
+end
+
+get "/events/:id/rsvps/new" do
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    puts @event.inspect
+    view "new_rsvp"
+end
+
+get "/events/:id/rsvps/create" do
+    # do stuff
+    puts params.inspect
+    rsvps_table.insert( :event_id => params["id"],
+                        :going => params["going"],
+                        :name => params["name"],
+                        :email => params["email"],
+                        :comments => params["comments"])
+    view "create_rsvp"
+end
